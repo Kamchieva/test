0bca273da280d0e48f3e818c9c87a7696cccff5e
Aidan <150118075+Kamchieva@users.noreply.github.com>
2024-04-12 23:13:22 -0500

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,12 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+ shirt s = new shirt("White", 'M');
+
+ System.out.println(s.color);
+        System.out.println(s.size);
+
+    }
+
+}
